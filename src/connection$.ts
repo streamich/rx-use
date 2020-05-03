b36ede5696b3badf740a61efc36c411065ef5822
streamich <streamich@gmail.com>
2020-05-03 18:09:39 +0200

    feat: 🎸 add onLine$, connection$ and network$ observables

---
+++ src/connection$.ts
@@ -0,0 +1,46 @@
+import {window} from './window';
+import {BehaviorSubject, fromEvent} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+export interface ConnectionState {
+  downlink?: number;
+  downlinkMax?: number;
+  effectiveType?: string;
+  rtt?: number;
+  type?: string;
+}
+
+export interface Connection extends ConnectionState, EventSource {}
+
+export interface NavigatorWithConnection extends Navigator {
+  connection?: Connection;
+  mozConnection?: Connection;
+  webkitConnection?: Connection;
+}
+
+const navigator: NavigatorWithConnection | undefined = (window && (window.navigator as NavigatorWithConnection)) || undefined;
+const connection: Connection | undefined = (navigator && (navigator.connection || navigator.mozConnection || navigator.webkitConnection))
+  ?  (navigator.connection || navigator.mozConnection || navigator.webkitConnection)
+  : undefined;
+
+const state = (): ConnectionState => {
+  if (!connection) return {};
+  const { downlink, downlinkMax, effectiveType, type, rtt } = connection;
+  return {
+    downlink,
+    downlinkMax,
+    effectiveType,
+    type,
+    rtt,
+  };
+};
+
+export const connection$: BehaviorSubject<ConnectionState> = new BehaviorSubject(state());
+
+if (!!connection) {
+  fromEvent(connection, 'change')
+    .pipe(
+      map(state)
+    )
+    .subscribe(connection$);
+}
---
+++ src/network$.ts
@@ -0,0 +1,24 @@
+import {onLine$} from './onLine$';
+import {ConnectionState, connection$} from './connection$';
+import {BehaviorSubject, merge} from 'rxjs';
+import { skip, map } from 'rxjs/operators';
+
+export interface NetworkState extends ConnectionState {
+  since: Date;
+  onLine: boolean;
+}
+
+const state = (): NetworkState => ({
+  since: new Date(),
+  onLine: onLine$.getValue(),
+  ...connection$.getValue(),
+});
+
+export const network$: BehaviorSubject<NetworkState> = new BehaviorSubject(state());
+
+merge(
+  onLine$.pipe(skip(1)),
+  connection$.pipe(skip(1)),
+).pipe(
+  map(state),
+).subscribe(network$);
