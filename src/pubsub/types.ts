37a2a583cc54da5b0f9c43ebe9c9b4c925a3e144
streamich <streamich@gmail.com>
2025-08-26 23:44:59 +0200

    feat: 🎸 allow loopback in .pub() methods and add generics

--- src/pubsub/types.ts
+++ src/pubsub/types.ts
@@ -2,11 +2,27 @@ import type {Observable} from 'rxjs';
 
 export type TopicPredicate<Data = unknown> = string | number | ((topic: string | number, data: Data) => boolean);
 
-export interface PubSub {
+export interface PubSub<Data = unknown> {
   /** Whether the pubsub service supports binary Uint8Array payloads. */
   bin?: boolean;
-  pub: <Data = unknown>(topic: string | number, data: Data) => void;
-  sub$: <Data = unknown>(topicPredicate: TopicPredicate<Data>) => Observable<Data>;
+
+  /**
+   * Publish an event to a topic.
+   * @param topic The topic to publish to.
+   * @param data The data to publish.
+   * @param loopback Whether to loop back the event to the sender.
+   * @returns
+   */
+  pub: (topic: string | number, data: Data, loopback?: boolean) => void;
+
+  /**
+   * Subscribe to events on a topic.
+   * @param topicPredicate The topic to subscribe to.
+   * @returns An observable that emits events from the topic.
+   */
+  sub$: (topicPredicate: TopicPredicate<Data>) => Observable<Data>;
+
+
   end: () => void;
   end$: Observable<void>;
 }
